cd730f8c429a446f21b50597f9ed726111ac61b4
pand-oly <hoa98hoa@gmail.com>
2023-02-10 19:17:53 -0300

    feat(Search which dish did Maria order the most?): Created function most_requested_dish to search what is the most ordered dish from a person in which it is passed by parameter

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,6 @@
 import csv
+from typing import Dict, List
+from collections import Counter
 
 
 def read_csv(path_to_file: str):  # -> List[Dict]:
@@ -10,5 +12,18 @@ def read_csv(path_to_file: str):  # -> List[Dict]:
         return [product for product in data]
 
 
+def most_requested_dish(name: str, orders: List[Dict]) -> int:
+    """receive the parameters
+    * name type string - name of the customer you want to search for
+    * orders type list of dict - with restaurant order list
+
+    looks and returns up a person's most ordered dish in an order list"""
+
+    const = [order["pedido"] for order in orders if order["cliente"] == name]
+    return Counter(const).most_common(1)[0][0]
+
+
 def analyze_log(path_to_file: str):
     data = read_csv(path_to_file)
+
+    most_requested_by_maria = most_requested_dish("maria", data)
