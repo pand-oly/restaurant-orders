0a573f3d98bd31d5bc8093947555a37b94f8c143
pand-oly <hoa98hoa@gmail.com>
2023-02-10 19:12:04 -0300

    feat(Read csv): create function to reade file csv and return list of dicts with orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,14 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import csv
+
+
+def read_csv(path_to_file: str):  # -> List[Dict]:
+    """Responsible for reading the csv file"""
+    with open(path_to_file) as file:
+        data = csv.DictReader(
+            file, delimiter=",", fieldnames=["cliente", "pedido", "dia"]
+        )
+        return [product for product in data]
+
+
+def analyze_log(path_to_file: str):
+    data = read_csv(path_to_file)
