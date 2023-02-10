291abec8e598502ab69d4e9ce4d0d695a1f6a71c
pand-oly <hoa98hoa@gmail.com>
2023-02-10 19:20:49 -0300

    feat(How many times did arnaldo ask for hamburger?): Created function how_many_times__order_this_dish to search how many times the customer ordered the dish

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -23,7 +23,25 @@ def most_requested_dish(name: str, orders: List[Dict]) -> int:
     return Counter(const).most_common(1)[0][0]
 
 
+def how_many_times__order_this_dish(
+    dish: str, name: str, orders: List[Dict]
+) -> int:
+    """takes 3 parameters:
+    * dish type string - dish name for search
+    * name type string - name of the customer you want to search for
+    * orders type list of dict - with restaurant order list
+
+    returns how many times the customer ordered the dish"""
+
+    const = [order["pedido"] for order in orders if order["cliente"] == name]
+    return Counter(const)[dish]
+
+
 def analyze_log(path_to_file: str):
     data = read_csv(path_to_file)
 
     most_requested_by_maria = most_requested_dish("maria", data)
+
+    arnaldo_order_hamburguer = how_many_times__order_this_dish(
+        "hamburguer", "arnaldo", data
+    )
