328853d87a99b20ead737192164cc6eae8980aed
pand-oly <hoa98hoa@gmail.com>
2023-02-11 10:19:04 -0300

    feat(Requeriment 3): when adding a new order uses the ingredient in the inventory and when ordering the shopping list it returns based on usage

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,49 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.__orders = list()
+
+    def __use_ingredients(self, order):
+        ingredients = self.INGREDIENTS[order]
+
+        for ingredient in ingredients:
+            self.MINIMUM_INVENTORY[ingredient] -= 1
+
+    def __search_contains_ingredient(self, order):
+        ingredients = self.INGREDIENTS[order]
+        for ingredient in ingredients:
+            if self.MINIMUM_INVENTORY[ingredient] < 1:
+                return False
+        return True
 
     def add_new_order(self, customer, order, day):
-        pass
+        if self.__search_contains_ingredient(order):
+            self.__orders.append(
+                {"customer": customer, "order": order, "day": day}
+            )
+            self.__use_ingredients(order)
 
     def get_quantities_to_buy(self):
-        pass
+        shopping_list = dict()
+        for key in self.MINIMUM_INVENTORY:
+            if key == "queijo":
+                shopping_list[key] = 100 - self.MINIMUM_INVENTORY[key]
+            else:
+                shopping_list[key] = 50 - self.MINIMUM_INVENTORY[key]
+
+        return shopping_list
